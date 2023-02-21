9ea8832ddb9399560dc7b7c3af686add492287b2
Levianeer <kingstonreecelane@gmail.com>
2023-02-22 04:32:27 +1100

    1.4.0
    Major structural changes

---
+++ src/data/scripts/fsdf_ModPlugin.java
@@ -0,0 +1,12 @@
+package data.scripts;
+
+import com.fs.starfarer.api.BaseModPlugin;
+import com.fs.starfarer.api.Global;
+import data.scripts.world.fsdf_WorldGen;
+
+public class fsdf_ModPlugin extends BaseModPlugin {
+    @Override
+    public void onNewGame() {
+        new fsdf_WorldGen().generate(Global.getSector());
+    }
+}
